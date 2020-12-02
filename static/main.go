48af71539356904e11352435b0fff9512921f203
Akira Hayashi <smxshxishxad@me.com>
2020-12-02 15:44:28 +0900

    :recycle: Use http.HandlerFunc()
    Resolve #3

--- static/main.go
+++ static/main.go
@@ -7,23 +7,15 @@ import (
 )
 
 
-type mesageHandler struct {
-	message string
-}
-
-func (m *mesageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, m.message)
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to Go Web Development")
 }
 
 func main() {
         mux := http.NewServeMux()
 
-	mh1 := &mesageHandler{"Welcome to Go Web Development"}
-	mux.Handle("/welcome", mh1)
-
-
-	mh2 := &mesageHandler{"net/http is awesome"}
-	mux.Handle("/message", mh2)
+	mh := http.HandlerFunc(messageHandler)
+	mux.Handle("/welcome", mh)
 
 	log.Println("Listening...")
         http.ListenAndServe(":8080", mux)
