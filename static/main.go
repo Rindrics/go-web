bed60f532c76b6180ae569bf377f59a8758785d8
Akira Hayashi <smxshxishxad@me.com>
2020-12-02 15:34:20 +0900

    :up: Create custom handler
    Resolve #1

--- static/main.go
+++ static/main.go
@@ -1,10 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+
+type mesageHandler struct {
+	message string
+}
+
+func (m *mesageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, m.message)
+}
 
 func main() {
         mux := http.NewServeMux()
-        fs := http.FileServer(http.Dir("public"))
-        mux.Handle("/", fs)
+
+	mh1 := &mesageHandler{"Welcome to Go Web Development"}
+	mux.Handle("/welcome", mh1)
+
+
+	mh2 := &mesageHandler{"net/http is awesome"}
+	mux.Handle("/message", mh2)
+
+	log.Println("Listening...")
         http.ListenAndServe(":8080", mux)
 }
