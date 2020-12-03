02e90cda131f4bbad926a00d5508bd4b722d9b1d
Akira Hayashi <smxshxishxad@me.com>
2020-12-03 13:51:37 +0900

    :up: Generate HTML using html/template
    Resolve #12

--- template/main.go
+++ template/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"os"
-	"text/template"
 )
 
 func main() {
 	t, err := template.New("test").Parse(`{{define "T"}}{{ $greet := "Hello" }}{{ $greet }}, {{.}}!
 {{ $greet }} again. {{end}}`)
-	err = t.ExecuteTemplate(os.Stdout, "T", "World")
+	err = t.ExecuteTemplate(os.Stdout, "T", "<a>Hello</a>")
 	if err != nil {
 		log.Fatal("Execute: ", err)
 		return
