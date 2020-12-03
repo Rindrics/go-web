27e822dd5a32633fc9baa266d1adee9961d3c670
Akira Hayashi <smxshxishxad@me.com>
2020-12-03 12:40:25 +0900

    :up: Use named tamplate
    Resolve #10

--- template/main.go
+++ template/main.go
@@ -6,30 +6,10 @@ import (
 	"text/template"
 )
 
-type Note struct {
-	Title		string
-	Description	string
-}
-
-const tmpl = `Notes  are:
-{{range .}}
-	Title: {{.Title}}, Description: {{.Description}}
-{{end}}`
-
 func main() {
-	notes := []Note{
-		{"text/template", "Template generates textual output"},
-		{"html/template", "Template generates HTML output"},
-	}
-
-	t := template.New("note")
-
-	t, err := t.Parse(tmpl)
+	t, err := template.New("test").Parse(`{{define "T"}}Hello, {{.}}!{{end}}`)
+	err = t.ExecuteTemplate(os.Stdout, "T", "World")
 	if err != nil {
-		log.Fatal("Parse: ", err)
-		return
-	}
-	if err := t.Execute(os.Stdout, notes); err != nil {
 		log.Fatal("Execute: ", err)
 		return
 	}
