fffac0c2235e1df6721043ba2c1e7750b82717fc
Akira <smxshxishxad@me.com>
2020-12-05 11:44:09 +0900

    :up: Chain middleware
    Resolve #16

--- middleware/main.go
+++ middleware/main.go
@@ -5,17 +5,34 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 )
-
-func loggingHandler (next http.Handler) http.Handler {
+func middlewareFirst(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		log.Printf("Started %s %s", r.Method, r.URL.Path)
+		log.Println("MiddlewareFirsvt  - Before Handler")
 		next.ServeHTTP(w, r)
-		log.Printf("Completed %s in %v", r.URL.Path, time.Since(start))
+		log.Println("MiddlewareFirst - After Handler")
+
 	})
 }
+
+func middlewareSecond(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("MiddlewareSecond - Before Handler")
+		if r.URL.Path == "/message" {
+			if r.URL.Query().Get("password") == "pass123" {
+				log.Println("Authorized to the system")
+				next.ServeHTTP(w, r)
+			} else {
+				log.Println("Failed to authorize to the system")
+				return
+			}
+		} else {
+			next.ServeHTTP(w, r)
+		}
+		log.Println("MiddlewareSecond - After Handler")
+	})
+}
+
 
 func index(w http.ResponseWriter, r *http.Request) {
 	log.Println("Executing index handler")
@@ -27,15 +44,18 @@ func about(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Go Middleware")
 }
 
+func message(w http.ResponseWriter, r *http.Request) {
+	log.Println("Executing message Handler")
+	fmt.Fprintf(w, "HTTP Middleware is awesome")
+}
+
 func iconHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
 	http.HandleFunc("/favicon.ico", iconHandler)
-	indexHandler := http.HandlerFunc(index)
-	aboutHandler := http.HandlerFunc(about)
-	http.Handle("/", loggingHandler(indexHandler))
-	http.Handle("/about", loggingHandler(aboutHandler))
+	http.Handle("/", middlewareFirst(middlewareSecond(http.HandlerFunc(index))))
+	http.Handle("/message", middlewareFirst(middlewareSecond(http.HandlerFunc(message))))
 	server := &http.Server {
 		Addr: ":8080",
 	}
