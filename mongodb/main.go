2bd574cd342a2f298677354319993fdb438d2a46
Akira <smxshxishxad@me.com>
2021-01-17 22:57:13 +0900

    :up: Record data using map and document slice
    Resolve #24

--- mongodb/main.go
+++ mongodb/main.go
@@ -8,45 +8,35 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-type Category struct {
-	Id          bson.ObjectId `bson:"_id,omitempty"`
-	Name        string
-	Description string
-}
-
 func main() {
 	session, err := mgo.Dial("localhost")
 	if err != nil {
 		panic(err)
 	}
 	defer session.Close()
-	// Optional. Switch the session to a monotonic behavior.
-	// Reads may not be entirely up-to-date, but they will always see the
-	// history of changes moving forward, the data read will be consistent
-	// across sequential queries in the same session, and modifications made
-	// within the session will be observed in following queries (read-your-writes).
-	// http://godoc.org/labix.org/v2/mgo#Session.SetMode
 	session.SetMode(mgo.Monotonic, true) //get collection
+
 	c := session.DB("taskdb").C("categories")
-	doc := Category{
-		bson.NewObjectId(),
-		"Open Source",
-		"Tasks for open-source projects",
+
+	docM := map[string]string{
+		"name": "Open Source",
+		"description": "Tasks for open-source projects",
 	}
-	//insert a category object
-	err = c.Insert(&doc)
+
+	//insert a map object
+	err = c.Insert(docM)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//insert two category objects
-	err = c.Insert(
-		&Category{bson.NewObjectId(), "R & D", "R & D Tasks"},
-		&Category{bson.NewObjectId(), "Project", "Project Tasks"},
-	)
-
+	docD := bson.D{
+		{"name", "Project"},
+		{"description", "Project Tasks"},
+	}
+	//insert a document slice
 	var count int
 	count, err = c.Count()
+	err = c.Insert(docD)
 	if err != nil {
 		log.Fatal(err)
 	} else {
